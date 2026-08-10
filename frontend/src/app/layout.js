33cee1e84a5cde66c5c740c28d13bd757e5063ff
Mehak Iqbal <mehakiqbal974@gmail.com>
2026-08-10 21:18:20 +0500

    fix: remove Monetag popunder delay for immediate loading

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -93,7 +93,7 @@ export default async function RootLayout({ children }) {
         <link rel="alternate" hrefLang="zh" href={SITE_URL + "/"} />
         <link rel="alternate" hrefLang="x-default" href={SITE_URL + "/"} />
         <meta name="6a97888e-site-verification" content="96070b758f3aa1bd8cc49f6ef180d595" />
-        <script dangerouslySetInnerHTML={{ __html: `setTimeout(function(){var s=[document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script'));s.dataset.zone='11533092',s.src='https://al5sm.com/tag.min.js'},300000);` }} />
+        <script dangerouslySetInnerHTML={{ __html: `(function(s){s.dataset.zone='11533092',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))` }} />
       </head>
       <body className={`${dmSans.className} dark bg-bg`} suppressHydrationWarning>
         <JsonLd data={organizationSchema()} />
