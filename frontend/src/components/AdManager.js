b659f4e6521a9b618457375cf784dfe6943c05b5
mehak iqbal <mehakiqbal974@gmail.com>
2026-08-24 16:10:16 +0500

    Replace inline ad script with external ads.js

--- frontend/src/components/AdManager.js
+++ frontend/src/components/AdManager.js
@@ -4,9 +4,14 @@ import { useEffect } from "react";
 
 export default function AdManager() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.text = `(function(iib){var d = document,s = d.createElement('script'),l = d.scripts[d.scripts.length - 1];s.settings = iib || {};s.src = "\\/\\/expensive-pollution.com\\/csD.9-6MbC2B5OlRSHWhQH9pN\\/zvMTyIMDD\\/UjyiOqSz0\\/3TMQzaILwqNWT\\/Mizk";s.async = true;s.referrerPolicy = 'no-referrer-when-downgrade';l.parentNode.insertBefore(s, l);})({})`;
-    document.head.appendChild(script);
+    fetch("/ads.js")
+      .then((res) => res.text())
+      .then((text) => {
+        const script = document.createElement("script");
+        script.text = text;
+        document.head.appendChild(script);
+      })
+      .catch(() => {});
   }, []);
 
   return null;
